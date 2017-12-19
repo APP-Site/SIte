651fa3fd8c4c3408651d185c60616238ee1ca867
Gak47bosniak <gaelchereau@gmail.com>
2017-12-19 15:56:42 +0100

    yeye si ca marche je m'autoo s _ _ _

--- vues/vue_erreur_inscription.php
+++ vues/vue_erreur_inscription.php
@@ -7,7 +7,7 @@
      </head>
      <body id="erreur">
      	<p>
-     		Une erreure c'est produite. Veuillez <a href="vue_verification_inscription" id="verifier">vérifier</a> votre email et votre code d'accès donné à l'inscription.
+     		trop d'erreures Une erreure c'est produite. Veuillez <a href="vue_verification_inscription" id="verifier">vérifier</a> votre email et votre code d'accès donné à l'inscription.
      	</p>
 
      </body>
