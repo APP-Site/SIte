a999e54ac5f221f0a1f324e0c43f614fb1fbf918
gaeldesmontagnes <laura.guer@sfr.fr>
2017-12-30 14:56:41 +0100

    f

--- vues/editeur1.php
+++ vues/editeur1.php
@@ -8,27 +8,27 @@
             <link rel="stylesheet" href="editeur.css"/>
            <title>Techn'O'Logis-Tableau de Bord</title>
      </head>
-    
-    <body class="body">    
-      
-              
+
+    <body class="body">
+
+
         <?php include("hpage.php"); ?>
-       
+
         <div class ="corps_edit">
-  
+
         <?php include("notification.php"); ?>
         <section class = "intro">
-        <p class="bjr"> 
-        Bonjour <?php echo $_SESSION['prenom'];?> <?php  echo $_SESSION['nom']?> ! 
+        <p class="bjr">
+        Bonjour <?php echo $_SESSION['prenom'];?> <?php  echo $_SESSION['nom']?> !
         </p>
         <p class = "bienvenue">
         Bienvenue dans l'editeur de votre maison, 1er etape de conception de votre espace. Vous aurez a repertorier les differentes pieces de votre logis en etant le plus exact possible. Vous n'aurez plus qu'en suite a choisir parmis notre large selection de capteurs pour les placer dans vos pieces!
         </p><br>
       	<hr class= "ligne">
-     
+
         <p class="logis">
         Type de logis :
-        </p>  
+        </p>
 <form class = "check">
 	<div class ="c1">
     <input type="radio" id="choix1"
@@ -40,36 +40,36 @@
      name="contact" value="Appartement">
     <label for="choix2">Appartement</label>
     </div>
-  
+
     <div>
     <button class="validity" type="submit">Valider</button>
     </div>
 </form>
 	<hr class ="ligne1">
-	<p class="pièces">
+	<p class="piï¿½ces">
 	Pieces a ajouter:
 	</p>
-	
+
 	<form method="post" action="../Controleur/controleur_editeur.php">
 	<div class = "manychoices">
    	<article>
-       <input type="checkbox" name="Salon[]" id="salon" /> 
+       <input type="checkbox" name="Salon[]" id="salon" />
        <label for="salon">Salon</label>
        <select class="salon" 	name="salon" id="salon">
        	   <option value="0">0</option>
            <option value="1">1</option>
            <option value="2">2</option>
            <option value="3">3</option></select><br/>
-           
-       <input type="checkbox" name="cuisine[]" id="cuisine" /> 
-       <label for="cuisine">Cuisine</label> 
+
+       <input type="checkbox" name="cuisine[]" id="cuisine" />
+       <label for="cuisine">Cuisine</label>
        <select class="cuisine" 	name="cuisine" id="cuisine">
        	   <option value="0">0</option>
            <option value="1">1</option>
            <option value="2">2</option>
            <option value="3">3</option></select><br/>
-           
-       <input type="checkbox" name="toilettes[]" id="toilettes" /> 
+
+       <input type="checkbox" name="toilettes[]" id="toilettes" />
        <label for="toilettes">Toilettes</label>
        <select class="toilettes" 	name="toilettes" id="toilettes">
        	   <option value="0">0</option>
@@ -78,9 +78,9 @@
            <option value="3">3</option>
        </select><br/>
     </article>
-    
+
     <article>
-       <input type="checkbox" name="chambre[]" id="chambre" /> 
+       <input type="checkbox" name="chambre[]" id="chambre" />
        <label for="chambre">Chambre</label>
        <select class="chambre" 	name="chambre" id="chambre">
        	   <option value="0">0</option>
@@ -90,8 +90,8 @@
            <option value="4">4</option>
            <option value="5">5</option>
        </select><br>
-       
-      <input type="checkbox" name="bureau[]" id="bureau" /> 
+
+      <input type="checkbox" name="bureau[]" id="bureau" />
       <label for="bureau">Bureau</label>
          <select class="bureau" 	name="bureau" id="bureau">
        	   <option value="0">0</option>
@@ -99,8 +99,8 @@
            <option value="2">2</option>
            <option value="3">3</option>
        </select><br>
-       
-       <input type="checkbox" name="salle_de_bain[]" id="salle_de_bain"/> 
+
+       <input type="checkbox" name="salle_de_bain[]" id="salle_de_bain"/>
        <label for="salle_de_bain">Salle de bain </label>
        <select class="salle_de_bain" 	name="salle_de_bain" id="bureau">
        	   <option value="0">0</option>
@@ -109,8 +109,8 @@
            <option value="3">3</option>
        </select><br>
      </article>
-      
-      
+
+
     <button class="validity1" type="submit">Valider</button>
       <form method="post" action="traitement.php">
 
@@ -119,10 +119,12 @@
 	</form>
 	<hr class = "ligne2">
 
-     
-       
+
+
      </div>
-        
+
         <?php include("ppage.php"); ?>
-        
+
     </body>
+
+    hhh 
