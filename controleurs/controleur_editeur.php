820f593c7ba680d6ca9892b2563cce4d0f9c0e35
PierreL78 <pierre.legentil78@gmail.com>
2018-01-08 15:42:50 +0100

    abcd

--- controleurs/controleur_editeur.php
+++ controleurs/controleur_editeur.php
@@ -1,26 +1,15 @@
 <?php
 
-try
-{
-    $bdd = new PDO('mysql:host=localhost:3306;dbname=domisep;charset=utf8', 'root', '');
-    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (Exception $e)
-{
-    die('Erreur : ' . $e->getMessage());
-}
+include(../modele/modele_connexion_bdd.php);
 
-$req = $bdd->prepare("INSERT INTO piece(salon, cuisine, toilettes, chambre, bureau, salle_de_bain) VALUES(:salon, :cuisine, :toilettes, :chambre, :bureau, :salle_de_bain)");
+$req = $bdd->prepare("INSERT INTO piece(nom) VALUES(:nom)");
 
 $req->execute(array(
 
-':salon' => $_POST['salon'],
-':cuisine' => $_POST['cuisine'],
-':toilettes' => $_POST['toilettes'],
-':chambre' => $_POST['chambre'],
-':bureau' => $_POST['bureau'],
-':salle_de_bain' => $_POST['salle_de_bain']));
 
-echo 'cc';
+':nom' => $_POST['nom'],
+));
+
+echo $_POST['nom'];
 $req -> closeCursor();
-
+?>
