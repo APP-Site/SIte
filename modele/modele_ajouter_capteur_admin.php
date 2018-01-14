32bbd0edd6b4971ef9e940fbe571050ef2ac11e0
gaeldesmontagnes <laura.guer@sfr.fr>
2018-01-14 17:16:20 +0100

    Admin l

--- modele/modele_ajouter_capteur_admin.php
+++ modele/modele_ajouter_capteur_admin.php
@@ -2,9 +2,14 @@
 
 include('../modele/modele_connexion_bdd.php');
 
-$req = $bdd->prepare("INSERT INTO capteur_actionneur(reference, statut) VALUES(:reference, :capteur)");
+$req = $bdd->prepare("INSERT INTO capteur_actionneur(type, unite, reference, image, statut) VALUES(:type, :unite, :reference, :image, :statut)");
 $req->execute(array(
-    ':reference' => $_POST['reference'],
-    ':capteur' => "capteur"
-
+    ':type' => $_POST['type'],
+    ':unite' => $_POST['unite'],
+    ':reference' => $_POST['ref'],
+    ':image' => $_POST['image'],
+    ':type' => $_POST['type'],
+    ':statut' => $_POST['statut']
 ));
+
+?>
