9ec03123a666c7811dfbafd9ad07bb9f0acb67e2
PierreL78 <pierre.legentil78@gmail.com>
2018-01-15 14:20:32 +0100

    fgb

---
+++ G5B - LEGENTIL/formulaire_2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>information sur vos séries</title>
+</head>
+<body>
+<p> séléctionner une catégorie: </p>
+<form method="post" action="formulaire_3.php">
+	<select>
+		<option value="1">Action</option>
+		<option value="2">Drama</option>
+		<option value="3">Horreur</option>
+		<option value="4">Humoristique</option>
+		<option value="5">Fantastique</option>
+		<input type="submit" name="envoi" value="Envoyer">
+	</select>
+</form>
+ <p> les catégories seront listés ici
+	<a href="formulaire_3.php">Votre compte</a></p>
+</body>gb
+</html>
