70f5a80240c3fa3f1b4d7859c1590402352121b6
tianyang Ma <matianyang627@gmail.com>
2018-01-15 14:34:29 +0100

    v

--- vues/vue_tableau_de_bord.php
+++ vues/vue_tableau_de_bord.php
@@ -7,6 +7,7 @@
             <meta charset="UTF-8"/>
             <link rel="stylesheet" href="style.css"/>
            <title>Techn'O'Logis-Tableau de Bord</title>
+          
      </head>
 
     <body>
