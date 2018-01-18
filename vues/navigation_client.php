fbd6d8657677970fa2b547fbe7fcf1ac46da50f0
Gak47bosniak <gaelchereau@gmail.com>
2018-01-18 11:44:29 +0100

    MAJ mise en page et image tdb

--- vues/navigation_client.php
+++ vues/navigation_client.php
@@ -1,6 +1,6 @@
 <nav>
 	<ul class="nav">
-		<li><?php echo $_SESSION['prenom']?> <?php echo $_SESSION['nom']?></li>
+		<li class="nav_utilisateur"><?php echo $_SESSION['prenom']?> <?php echo $_SESSION['nom']?></li>
 		<li class="partie"><a href="vue_tableau_de_bord.php?titre=Tableau de bord">Tableau de bord</a></li>
 		<li class="partie"><a href="vue_profil.php?titre=Profil">Profil</a></li>
 		<li class="partie"><a href="vue_information.php?titre=Informations">Informations</a></li>
